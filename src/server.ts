cb353d6dec493df1b2be01091d41c89e576194fc
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-10-22 14:02:56 +0200

    I'm proud of this commit ! _Authorisation in progress

--- src/server.ts
+++ src/server.ts
@@ -59,63 +59,66 @@ export function makeApp(mongoClient: MongoClient): core.Express {
 
   const oauthClient = new OAuth2Client(oauthClientConstructorProps);
 
-
-   app.get("/oauth/callback", sessionParser, (request: Request, response: Response) => {
-    // get back an Access Token from an OAuth2 Authorization Code
-    if (request.session) {
-      request.session.accessToken = token.access_token;
-    }
-    response.redirect("/loggued-in-part-of-your-app");
-  }); 
-
   app.get("/login", async (_request, response) => {
     const authURL = await oauthClient.getAuthorizationURL("state");
 
     const authURLinString = authURL.toString();
     response.redirect(authURLinString);
   });
+  app.get("/logout", sessionParser, async (_request, response) => {
+    if (_request.session) {
+      _request.session.destroy(() => {
+        response.render("pages/home", { isLoggedIn: false });
+      });
+    }
+  });
 
-
-  /* http://localhost:8080/oauth/callback?code=zwKc_0DnGn-77tTrm8rJSQ%3D%3D&state=state */
   app.get("/oauth/callback", sessionParser, async (request, response) => {
     const stringiAuthCode = `${request.query.code}`;
     const token = await oauthClient.getTokensFromAuthorizationCode(stringiAuthCode);
     if (request.session) {
       request.session.accessToken = token.access_token;
     }
-    response.redirect("/loggued-in");
+    response.redirect("/");
   });
 
-  app.get("/loggued-in", sessionParser, async (request, response) => {
+  app.get("/", sessionParser, async (request, response) => {
     if (!request.session || !request.session.accessToken) {
-      // Can't find the session
-      response.redirect("/login");
+      response.render("pages/home", { isLoggedIn: false });
+      console.log("you are not conected");
       return;
     }
-    response.render("pages/loggued-in");
+    try {
+      await oauthClient.verifyJWT(request.session.accessToken, process.env.JWT_ALGORITHM || "");
+      response.render("pages/home", { isLoggedIn: true });
+      console.log("you are conected");
+    } catch (error) {
+      request.session.destroy(() => {
+        response.render("pages/home", { isLoggedIn: false });
+        console.error(error);
+      });
+    }
   });
-  
-  app.get("/", (_request, response) => response.render("pages/home"));
   app.get("/api", (_request, response) => response.render("pages/api"));
 
-  app.get("/platforms", platformsController.index(platformModel));
-  app.get("/platforms/new", platformsController.newPlatform());
-  app.get("/platforms/:slug", platformsController.show(platformModel));
-  app.get("/platforms/:slug/edit", platformsController.edit(platformModel));
-  app.post("/platforms", jsonParser, formParser, platformsController.create(platformModel));
-  app.put("/platforms/:slug", jsonParser, platformsController.update(platformModel));
-  app.post("/platforms/:slug", formParser, platformsController.update(platformModel));
-  app.delete("/platforms/:slug", jsonParser, platformsController.destroy(platformModel));
+  app.get("/platforms", sessionParser, platformsController.index(platformModel));
+  app.get("/platforms/new", sessionParser, platformsController.newPlatform());
+  app.get("/platforms/:slug", sessionParser, platformsController.show(platformModel));
+  app.get("/platforms/:slug/edit", sessionParser, platformsController.edit(platformModel));
+  app.post("/platforms", sessionParser, jsonParser, formParser, platformsController.create(platformModel));
+  app.put("/platforms/:slug", sessionParser, jsonParser, platformsController.update(platformModel));
+  app.post("/platforms/:slug", sessionParser, formParser, platformsController.update(platformModel));
+  app.delete("/platforms/:slug", sessionParser, jsonParser, platformsController.destroy(platformModel));
 
-  app.get("/platforms/:slug/games", gamesController.list(gameModel));
-  app.get("/games", gamesController.index(gameModel));
-  app.get("/games/new", gamesController.newGame());
-  app.get("/games/:slug", gamesController.show(gameModel));
-  app.get("/games/:slug/edit", gamesController.edit(gameModel));
-  app.post("/games", jsonParser, formParser, gamesController.create(gameModel, platformModel));
-  app.put("/games/:slug", jsonParser, gamesController.update(gameModel, platformModel));
-  app.post("/games/:slug", formParser, gamesController.update(gameModel, platformModel));
-  app.delete("/games/:slug", jsonParser, gamesController.destroy(gameModel));
+  app.get("/platforms/:slug/games", sessionParser, gamesController.list(gameModel));
+  app.get("/games", sessionParser, gamesController.index(gameModel));
+  app.get("/games/new", sessionParser, gamesController.newGame());
+  app.get("/games/:slug", sessionParser, gamesController.show(gameModel));
+  app.get("/games/:slug/edit", sessionParser, gamesController.edit(gameModel));
+  app.post("/games", sessionParser, jsonParser, formParser, gamesController.create(gameModel, platformModel));
+  app.put("/games/:slug", sessionParser, jsonParser, gamesController.update(gameModel, platformModel));
+  app.post("/games/:slug", sessionParser, formParser, gamesController.update(gameModel, platformModel));
+  app.delete("/games/:slug", sessionParser, jsonParser, gamesController.destroy(gameModel));
 
   app.get("/*", (request, response) => {
     console.log(request.path);
